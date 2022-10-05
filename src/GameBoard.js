72f3567ed0b425249857d5b5f14b4ed6b3b98b6d
Amy Morrisett <morrisettamy@gmail.com>
2022-10-05 12:48:36 -0400

    added functionality to red spaces, blue spaces, skip event, and lucky space, still need to fix lucky space item condition

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,34 +1,76 @@
-//TODO: make database (postgres or firestore?)
 //store for a given game:
 //4 players -- for each one:
 //character, current coins, current stars, current items, current space, perhaps other stats for bonus stars?
-//status of each piranha plant event (who owns it if anyone, is it stealing coins or stars)
+//TODO: how to shift turns from one player to the next, how to represent different players' spaces on the board (probs with colors, or maybe putting their name after the space on the board)
+//TODO: account for board events
+//TODO: allow players to use items and factor those into the turn
+//TODO: status of each piranha plant event (who owns it if anyone, is it stealing coins or stars)
+//TODO: implement item shop
+//LATER GOAL: put in other boards?
+//we are not gonna have triple dice cause it will make my life harder lol
 
 import React, { useState, useEffect } from 'react';
 import { db } from './firebase';
 import { getDoc, doc, updateDoc } from 'firebase/firestore';
 
 function GameBoard() {
-  //L is lucky, B is blue, CE is coin piranha event, SE is star piranha event, KE is skip event, R is red, W is Bowser, C is chance, V is VS, S is star
+  //L is lucky, B is blue, CE is coin piranha event, SE is star piranha event, KE is skip event, R is red, W is Bowser, C is chance, V is VS
 
-  //after space 32, bowser flower lottery happens, if they "win" they go to the bowser detour spaces
-
-  //after space 22, players can buy a star if they have enough coins
   const [diceRoll, setDiceRoll] = useState(0);
   const [spaces, setSpaces] = useState([]);
   const [bowserDetour, setBowserDetour] = useState([]);
   const [playerInfo, setPlayerInfo] = useState({});
   const [wonTheLottery, setWonTheLottery] = useState(true);
 
-  async function handleDiceRoll() {
+  function handleDiceRoll() {
     setDiceRoll(Math.floor(Math.random() * 10) + 1);
   }
 
+  async function handleBoardEvent(player, spaceArr) {
+    // const gameDoc = doc(db, 'games', 'pmX2c0bJU9JNpY5wb4ZR');
+    // const gameDocSnap = await getDoc(gameDoc);
+    if (spaceArr[player.currentSpace.index] === 'B') {
+      player.coins += 3;
+    }
+    if (spaceArr[player.currentSpace.index] === 'R') {
+      player.coins -= 3;
+    }
+    if (spaceArr[player.currentSpace.index] === 'KE') {
+      player.currentSpace.index = 40;
+      alert('You skipped some spaces!');
+    }
+    // if (spaceArr[player.currentSpace.index] === 'CE') {
+    //     if (player.currentSpace.index === 2 || player.currentSpace.index === 3) {
+    //         let canUpgrade = gameDocSnap.data().p1.currentOwner ? true : false
+    //         updateDoc(gameDoc, {
+    //             p1: { currentOwner: player.charName, type:  },
+    //           });
+    //     }
+    //   }
+
+    if (spaceArr[player.currentSpace.index] === 'L') {
+      let randomChoice = Math.floor(Math.random() * 3);
+      if (randomChoice === 0) {
+        player.coins += 10;
+      }
+      if (randomChoice === 1) {
+        player.coins += 15;
+      }
+      //   let randomChoice = 2;
+      if (randomChoice === 2) {
+        const itemDoc = doc(db, 'party-info', 'itemPrices');
+        const itemDocSnap = await getDoc(itemDoc);
+        const itemArr = Object.keys(itemDocSnap.data());
+        player.items.push(itemArr[Math.floor(Math.random() * itemArr.length)]);
+        //TODO: right now this doesn't update until the next time we roll the dice & move
+      }
+      //options: get 10 coins, get 15 coins, get random item
+    }
+  }
+
   function handleUpdate() {
-    //const gameDocSnap = await getDoc(gameDoc);
-    const gameDoc = doc(db, 'games', 'pmX2c0bJU9JNpY5wb4ZR');
-    //console.log('playerInfo before roll', playerInfo);
     let updatedIndex = playerInfo.currentSpace.index + diceRoll;
+
     let updatedPlayer = {
       charName: playerInfo.charName,
       coins: playerInfo.coins,
@@ -39,12 +81,13 @@ function GameBoard() {
         index: updatedIndex,
       },
     };
-    //console.log('updatedIndex', updatedIndex);
+
     if (!playerInfo.currentSpace.bowserDetour) {
       if (updatedIndex >= 22 && updatedPlayer.coins >= 20) {
         updatedPlayer.coins -= 20;
         updatedPlayer.stars++;
       }
+
       if (playerInfo.currentSpace.index <= 32 && updatedIndex > 32) {
         let winningTheLottery = Math.floor(Math.random() * 4);
         if (!winningTheLottery) {
@@ -68,6 +111,7 @@ function GameBoard() {
           updatedPlayer.currentSpace.index -= 42;
         }
       }
+
       if (updatedIndex > 41) {
         updatedPlayer.coins += 10;
         updatedPlayer.currentSpace.index -= 42;
@@ -80,32 +124,26 @@ function GameBoard() {
         const receiveSlowDice = Math.floor(Math.random() * 2);
         if (receiveSlowDice) updatedPlayer.items.push('slow dice');
       }
+
       if (updatedIndex > 10) {
         setWonTheLottery(true);
         updatedPlayer.currentSpace.bowserDetour = false;
         updatedPlayer.currentSpace.index += 8;
       }
     }
+
     setPlayerInfo(updatedPlayer);
+
+    let currentBoard = updatedPlayer.currentSpace.bowserDetour
+      ? bowserDetour
+      : spaces;
+    handleBoardEvent(updatedPlayer, currentBoard);
+
+    const gameDoc = doc(db, 'games', 'pmX2c0bJU9JNpY5wb4ZR');
     updateDoc(gameDoc, {
       char1: { updatedPlayer },
     });
-    //if on normal board and player's index is currently 32 or less BUT diceRoll + player's index will be greater than 32, do the bowser lottery
-    //for right now, 3/4 chance of flipping it to true
-    //if it remains false, check if diceRoll + player's index will be greater than 41
-    //if  so, go back around (i.e. if you were on 40 and roll a 2, go to zero)
-    //if on bowser detour board and player's index is currently 10 or less BUT diceRoll + player's index will be greater than 10, make sure to set them back to the normal board (starting at space 19)
-
-    //need to find out if player will lap around, pass a star, do the bowser lottery, get the extra coins for making it around, etc.
   }
-
-  //   function displayBoard() {
-  //     // handleUpdate();
-  //     const gameDoc = doc(db, 'games', 'pmX2c0bJU9JNpY5wb4ZR');
-  //     updateDoc(gameDoc, {
-  //       char1: { playerInfo },
-  //     });
-  //   }
 
   useEffect(() => {
     const boardDoc = doc(db, 'party-info', 'boardLayouts');
@@ -126,6 +164,17 @@ function GameBoard() {
 
   return (
     <div className="GameBoard">
+      <div>
+        <h3>{playerInfo.charName} Info</h3>
+        <div>Coins: {playerInfo.coins}</div>
+        <div>Stars: {playerInfo.stars}</div>
+        <div>
+          Items:{' '}
+          {playerInfo.items
+            ? playerInfo.items.map((item) => <span>{item}, </span>)
+            : ''}
+        </div>
+      </div>
       <div>
         {spaces.map((space, idx) =>
           idx === playerInfo.currentSpace.index &&
@@ -148,7 +197,6 @@ function GameBoard() {
       </div>
       <button type="button" onClick={handleDiceRoll}>
         Roll Dice
-        {/* {console.log(spaces.indexOf('KE'))} */}
       </button>
       <p>{diceRoll ? `You rolled a(n) ${diceRoll}!` : 'Roll the dice!'}</p>
       <button type="button" onClick={handleUpdate}>
