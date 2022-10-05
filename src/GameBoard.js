e96ca58c5677a95da994901665982dcd643c4ee5
Amy Morrisett <morrisettamy@gmail.com>
2022-10-04 23:14:01 -0400

    bolded space updated correctly after clicking view updated board, but player index not updating correctly in firestore until clicking view updated board the next time

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -6,7 +6,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { db } from './firebase';
-import { getDoc, doc } from 'firebase/firestore';
+import { getDoc, doc, updateDoc } from 'firebase/firestore';
 
 function GameBoard() {
   //L is lucky, B is blue, CE is coin piranha event, SE is star piranha event, KE is skip event R is red, W is Bowser, C is chance, V is VS, S is star
@@ -18,6 +18,46 @@ function GameBoard() {
   const [spaces, setSpaces] = useState([]);
   const [bowserDetour, setBowserDetour] = useState([]);
   const [playerInfo, setPlayerInfo] = useState({});
+
+  async function handleDiceRoll() {
+    setDiceRoll(Math.floor(Math.random() * 10) + 1);
+  }
+
+  async function displayBoard() {
+    const gameDoc = doc(db, 'games', 'pmX2c0bJU9JNpY5wb4ZR');
+    //const gameDocSnap = await getDoc(gameDoc);
+    console.log('playerInfo before roll', playerInfo);
+    let updatedIndex = playerInfo.currentSpace.index + diceRoll;
+    console.log('updatedIndex', updatedIndex);
+    if (!playerInfo.currentSpace.bowserDetour) {
+      if (updatedIndex <= 32) {
+        console.log('should be seeing me every time');
+        const updatedPlayer = {
+          charName: playerInfo.charName,
+          coins: playerInfo.coins,
+          items: playerInfo.items,
+          stars: playerInfo.stars,
+          currentSpace: { bowserDetour: false, index: updatedIndex },
+        };
+        setPlayerInfo(updatedPlayer);
+      }
+
+      updateDoc(gameDoc, {
+        char1: { playerInfo },
+      });
+
+      //   setPlayerInfo(gameDocSnap.data().char1);
+      //   console.log('playerInfo after roll', playerInfo);
+    }
+
+    //if on normal board and player's index is currently 32 or less BUT diceRoll + player's index will be greater than 32, do the bowser lottery
+    //for right now, 3/4 chance of flipping it to true
+    //if it remains false, check if diceRoll + player's index will be greater than 41
+    //if  so, go back around (i.e. if you were on 40 and roll a 2, go to zero)
+    //if on bowser detour board and player's index is currently 10 or less BUT diceRoll + player's index will be greater than 10, make sure to set them back to the normal board (starting at space 19)
+
+    //need to find out if player will lap around, pass a star, do the bowser lottery, get the extra coins for making it around, etc.
+  }
 
   useEffect(() => {
     const boardDoc = doc(db, 'party-info', 'boardLayouts');
@@ -47,7 +87,7 @@ function GameBoard() {
             <span>{`${space}    `}</span>
           )
         )}
-        {console.log(playerInfo)}
+        {/* {console.log(diceRoll, playerInfo)} */}
       </div>
       <div>
         {bowserDetour.map((space, idx) =>
@@ -59,14 +99,15 @@ function GameBoard() {
           )
         )}
       </div>
-      <button
-        type="button"
-        onClick={() => setDiceRoll(Math.floor(Math.random() * 10) + 1)}
-      >
+      <button type="button" onClick={handleDiceRoll}>
         Roll Dice
-        {console.log(spaces.indexOf('KE'))}
+        {/* {console.log(spaces.indexOf('KE'))} */}
       </button>
-      <p>{diceRoll ? diceRoll : 'Roll the dice to see how far you will go!'}</p>
+      <button type="button" onClick={displayBoard}>
+        View Updated Board
+        {/* {console.log(spaces.indexOf('KE'))} */}
+      </button>
+      {/* <p>{diceRoll ? diceRoll : 'Roll the dice to see how far you will go!'}</p> */}
     </div>
   );
 }
